9815d5645a913de48cc9ec208c7f56590883fa45
anhduc2708 <68212055+anhduc2708@users.noreply.github.com>
2023-01-10 19:16:47 +0700

    Finish 80% Homepage UI with textview button transite to the product list frm

--- app/src/main/java/com/example/happytechhomepageui/HomeFragment.java
+++ app/src/main/java/com/example/happytechhomepageui/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.happytechhomepageui;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
@@ -8,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -29,7 +31,7 @@ import java.util.List;
 public class HomeFragment extends Fragment {
     private DatabaseHelper db;
     ImageSlider imageSlider;
-
+    TextView product;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -46,8 +48,8 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container,false);
-        Button cat1 = (Button) view.findViewById(R.id.cat1);
-        cat1.setOnClickListener(new View.OnClickListener() {
+        product = (TextView) view.findViewById(R.id.seeAll);
+        product.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
