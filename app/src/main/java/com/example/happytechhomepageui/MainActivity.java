5cf65d0fc5c4462a742a3adbfd1b3095b75ebf6f
Trung Tín <s3752975@rmit.edu.vn>
2023-01-17 00:45:40 +0700

    Fixing bottom item to change color base on the current fragment

--- app/src/main/java/com/example/happytechhomepageui/MainActivity.java
+++ app/src/main/java/com/example/happytechhomepageui/MainActivity.java
@@ -4,12 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -19,66 +22,126 @@ public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
 
     HomeFragment homeFragment = new HomeFragment();
-    InfoFragment qrScannerFragment = new InfoFragment();
-    NotiFragment notiFragment = new NotiFragment();
-    ProfileFragment profileFragment = new ProfileFragment();
+    InfoFragment infoFragment = new InfoFragment();
     CartFragment cartFragment = new CartFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        ImageButton cartBtn = findViewById(R.id.cart_button);
+//        ImageButton cartBtn = findViewById(R.id.cart_button);
+        TextView fragmentName = findViewById(R.id.fragmentText);
+        ImageView logo = (ImageView) findViewById(R.id.imageView);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, homeFragment).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, homeFragment, "home").commit();
         //Check if current fragment is is not cartFragment
         getSupportFragmentManager().addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
             @Override
             public void onBackStackChanged() {
                 Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.frameLayout);
-                if (currentFragment instanceof CartFragment) {
-                    cartBtn.setVisibility(View.GONE);
-                } else {
-                    cartBtn.setVisibility(View.VISIBLE);
+                if (currentFragment instanceof HomeFragment) {
+                    fragmentName.setVisibility(View.VISIBLE);
+                    logo.setVisibility(View.GONE);
+                    fragmentName.setText("Home");
+
+                }
+                else if (currentFragment instanceof InfoFragment) {
+                    fragmentName.setVisibility(View.VISIBLE);
+                    logo.setVisibility(View.GONE);
+                    fragmentName.setText("Order Information");
+
+                }
+                else if (currentFragment instanceof CartFragment) {
+                    fragmentName.setVisibility(View.VISIBLE);
+                    logo.setVisibility(View.GONE);
+                    fragmentName.setText("Cart");
+
+                }
+                else if (currentFragment instanceof ProfileFragment) {
+                    fragmentName.setVisibility(View.GONE);
+                    logo.setVisibility(View.VISIBLE);
                 }
             }
         });
+
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.home:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, homeFragment).commit();
-                        cartBtn.setVisibility(View.VISIBLE);
+                        Fragment home = homeFragment;
+                        FragmentTransaction transaction1 = getSupportFragmentManager().beginTransaction();
+                        transaction1.replace(R.id.frameLayout, home, "home");
+                        transaction1.addToBackStack(null);
+                        transaction1.commit();
+                        fragmentName.setVisibility(View.VISIBLE);
+                        fragmentName.setText("Home");
+                        logo.setVisibility(View.GONE);
                         return true;
                     case R.id.info:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, qrScannerFragment).commit();
-                        cartBtn.setVisibility(View.VISIBLE);
+                        Fragment order = infoFragment;
+                        FragmentTransaction transaction2 = getSupportFragmentManager().beginTransaction();
+                        transaction2.replace(R.id.frameLayout, order, "order");
+                        transaction2.addToBackStack(null);
+                        transaction2.commit();
+                        fragmentName.setVisibility(View.VISIBLE);
+                        fragmentName.setText("Order Information");
+                        logo.setVisibility(View.GONE);
                         return true;
-                    case R.id.notification:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, notiFragment).commit();
-                        cartBtn.setVisibility(View.VISIBLE);
+                    case R.id.cart:
+                        Fragment cart = cartFragment;
+                        FragmentTransaction transaction3 = getSupportFragmentManager().beginTransaction();
+                        transaction3.replace(R.id.frameLayout, cart, "cart");
+                        transaction3.addToBackStack(null);
+                        transaction3.commit();
+                        fragmentName.setVisibility(View.VISIBLE);
+                        fragmentName.setText("Cart");
+                        logo.setVisibility(View.GONE);
                         return true;
                     case R.id.profile:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, profileFragment).commit();
-                        cartBtn.setVisibility(View.VISIBLE);
+                        Fragment profile = profileFragment;
+                        FragmentTransaction transaction4 = getSupportFragmentManager().beginTransaction();
+                        transaction4.replace(R.id.frameLayout, profile, "profile");
+                        transaction4.addToBackStack(null);
+                        transaction4.commit();
+                        fragmentName.setVisibility(View.GONE);
+                        fragmentName.setText("");
+                        logo.setVisibility(View.VISIBLE);
                         return true;
                 }
                 return false;
             }
         });
-        cartBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, cartFragment, "CART_FRAGMENT").commit();
-                cartBtn.setVisibility(View.GONE);
-
-            }
-        });
     }
-//
+
+    @Override
+    public void onBackPressed() {
+        Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.frameLayout);
+        if (currentFragment instanceof HomeFragment) {
+            finish();
+            return;
+        }
+        if (currentFragment instanceof CartFragment) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, homeFragment, "home").commit();
+            return;
+        }
+        if (currentFragment instanceof InfoFragment) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, homeFragment, "home").commit();
+            return;
+        }
+        if (currentFragment instanceof ProfileFragment) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, homeFragment, "home").commit();
+            return;
+        }
+
+        super.onBackPressed();
+    }
+    //
 //    @Override
 //    protected void onResume() {
 //        super.onResume();
